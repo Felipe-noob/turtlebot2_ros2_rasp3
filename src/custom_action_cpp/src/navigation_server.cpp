f0ef64a793bef6de67a9cd9684dd69e7c192fc86
Felipe-noob <felipelorenzzon@gmail.com>
2025-03-11 13:31:20 +0100

    compiles with listener code

--- src/custom_action_cpp/src/navigation_server.cpp
+++ src/custom_action_cpp/src/navigation_server.cpp
@@ -8,6 +8,27 @@
 #include "rclcpp_components/register_node_macro.hpp"
 
 #include "custom_action_cpp/visibility_control.h"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
 
 namespace custom_action_cpp {
 class NavigationActionServer : public rclcpp::Node {
@@ -51,6 +72,8 @@ public:
     this->action_server_ = rclcpp_action::create_server<UsineGoalPose>(
         this, "navigation", handle_goal, handle_cancel, handle_accepted);
   }
+
+
 
 private:
   rclcpp_action::Server<UsineGoalPose>::SharedPtr action_server_;
